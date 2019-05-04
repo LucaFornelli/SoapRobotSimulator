ae8afaff5bc2effa574b4c2e5a2caed57b2c987e
Luca Fornelli <lucamagnolie@gmail.com>
2019-05-04 17:00:03 +0200

    Initial commit SoapRobotSimulator

---
+++ src/main/java/it/luca/soaprobotsimulator/SoapRobotSimulatorApplication.java
@@ -0,0 +1,13 @@
+package it.luca.soaprobotsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapRobotSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapRobotSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/luca/SoapRobotSimulator/SoapRobotSimulatorApplicationTests.java
@@ -0,0 +1,16 @@
+package it.luca.SoapRobotSimulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapRobotSimulatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
