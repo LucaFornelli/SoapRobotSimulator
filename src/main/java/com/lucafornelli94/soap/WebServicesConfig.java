e9f2babbdcbb3a7a67e6c9bd4a93156ea16afc8c
Luca Fornelli <lucamagnolie@gmail.com>
2019-05-04 17:30:59 +0200

    Introduced wsdl4j and WebServicesConfig configuration file

---
+++ src/main/java/com/lucafornelli94/soap/WebServicesConfig.java
@@ -0,0 +1,41 @@
+package com.lucafornelli94.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServicesConfig {
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet, "/RobotSimulator/*");
+	}
+
+	// /ws/courses.wsdl
+	// course-details.xsd
+	@Bean(name = "robot")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		definition.setPortTypeName("RobotPort");
+		definition.setTargetNamespace("http://lucafornelli94.com/robot");
+		definition.setLocationUri("/RobotSimulator");
+		definition.setSchema(coursesSchema);
+		return definition;
+	}
+
+	@Bean
+	public XsdSchema coursesSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("robot-details.xsd"));
+	}
+}
